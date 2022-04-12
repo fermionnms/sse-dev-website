d47bfd8d47780b9d21914fed7f4d550811e53d07
wstucky <werner@stucky-solutions.net>
2022-04-12 14:20:58 +0200

    changed to port 3000

--- testsse.js
+++ testsse.js
@@ -2,7 +2,7 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const port = 6758
+const port = 3000;
 
 const fs = require('fs')
 const home = fs.readFileSync('home.html', 'utf8');
